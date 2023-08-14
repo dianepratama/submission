d65f92739473c5a7695bcbecf344f6554aecd014
dianepratama <diane.pratama@krakatausteel.com>
2023-08-14 11:34:02 +0700

    Form Blade Add

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('form');
+});
+
+Route::get('/post', function () {
+    return '<h2>post</h2>';
 });
