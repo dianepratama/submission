00592ef67625d9d8d8e70a89121a4dc3b0781bc7
dianepratama <diane.pratama@krakatausteel.com>
2023-08-14 14:02:04 +0700

    Create Yield + Component

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<title>Submission App</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://cdn.tailwindcss.com"></script>
+
+<body class="bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100 font-sans">
+
+    <x-navbar> </x-navbar>
+    <x-header> </x-header>
+    @yield('content')
+    <x-footer> </x-footer>
+
+
+</body>
+
+</html>
