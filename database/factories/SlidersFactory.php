55fcd73e9ca57a44c26e733873de928816ba99b7
dianepratama <diane.pratama@krakatausteel.com>
2023-08-14 16:36:38 +0700

    Status Model, Factory, & Seeder

---
+++ database/factories/SlidersFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sliders>
+ */
+class SlidersFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->name(),
+            'photo' => $this->faker->image(storage_path('images'), 600, 300),
+        ];
+    }
+}
